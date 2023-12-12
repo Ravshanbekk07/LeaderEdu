c60dae78840ae9041e76768519eb10f7079c218f
Ravshanbekk07 <ramazonof07@gmail.com>
2023-12-12 10:58:04 +0500

    registered teacher model

--- core/settings.py
+++ core/settings.py
@@ -149,6 +149,7 @@ SIMPLE_JWT = {
 
 }
 
+# AUTH_USER_MODEL = 'users.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- teachers/models.py
+++ teachers/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 class Teacher(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=100)
     description=models.CharField(null=True,blank=True)
     phone=models.CharField(max_length=13)
-    experience=models.IntegerField()
+    experience=models.FloatField()
     score=models.FloatField()
     picture=models.ImageField(upload_to='pictures',null=True,blank=True)
 
 
+
+    def __str__(self) -> str:
+        return self.name
