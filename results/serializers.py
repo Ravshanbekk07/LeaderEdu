4994f8e10cbe30611f32b110b7ef62348464607c
Ravshanbekk07 <ramazonof07@gmail.com>
2023-12-14 09:14:58 +0500

    results in uz

--- results/serializers.py
+++ results/serializers.py
@@ -1,11 +1,29 @@
 from rest_framework import serializers
 from teachers.models import Teacher
+from teachers.serializers import TeacherSerializer
 from .models import Results
 
 class ResultSerializer(serializers.ModelSerializer):
     class Meta:
         model=Results
-        fields='__all__'
+        fields="__all__"
+    # def to_representation(self, instance):
+    #     representation= super().to_representation(instance)
+    #     if instance.teacher:
+    #         representation['teacher_detail']=TeacherSerializer(instance.teacher).data
+    #     return representation
+class ResultSerializerUz(serializers.ModelSerializer):
+    class Meta:
+        model=Results
+        fields=['id','description_uz','picture','teacher']
+    def to_representation(self, instance):
+        representation= super().to_representation(instance)
+        if instance.teacher:
+            representation['teacher_detail']=TeacherSerializer(instance.teacher).data
+        return representation
 
-
+class ResultSerializerUz(serializers.ModelSerializer):
+    class Meta:
+        model=Results
+        fields=['id','description_uz','picture','teacher']
         
--- results/views.py
+++ results/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .models import Results
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ResultSerializer
+from .serializers import ResultSerializer,ResultSerializerUz
 from rest_framework.exceptions import PermissionDenied
 
 def check_user(role):
@@ -10,6 +10,12 @@ def check_user(role):
         raise PermissionDenied(detail='Only admins are allowed')
 
 class ResultList(APIView):
+    def get(self,request):
+        language=request.GET.get('language','ru')
+        if language =='uz':
+            results=Results.objects.all()
+            serializer=ResultSerializerUz(results,many=True)
+            return Response(serializer.data)
     def post(self,request):
         check_user(request.user.role)
         serializer=ResultSerializer(data=request.data)
