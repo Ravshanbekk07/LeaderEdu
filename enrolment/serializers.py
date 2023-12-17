6cf30bec0bebfcb6434a1a8d9dbb9181009697f0
Ravshanbekk07 <ramazonof07@gmail.com>
2023-12-17 12:41:42 +0500

    enrolment serializer

--- enrolment/serializers.py
+++ enrolment/serializers.py
@@ -14,8 +14,8 @@ class Enrolmenterializer(serializers.ModelSerializer):
 class EnrolmentSerializerUz(serializers.ModelSerializer):
     class Meta:
         model=Enrolment
-        fields=[]
+        fields=['id','name_uz','age','student_number','parents_number','course_language_uz','course_id','status']
 class EnrolmentSerializerRu(serializers.ModelSerializer):
     class Meta:
         model=Enrolment
-        fields=[]
+        fields=['id','name_ru','age','student_number','parents_number','course_language_ru','course_id','status']
