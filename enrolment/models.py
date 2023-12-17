3083fd972876cd4c9965bb7ad4f155e7a275c56e
Ravshanbekk07 <ramazonof07@gmail.com>
2023-12-17 14:37:43 +0500

    enrolment language

--- enrolment/models.py
+++ enrolment/models.py
@@ -25,11 +25,11 @@ class Enrolment(models.Model):
     student_numer=PhoneNumberField()
     parents_numer=PhoneNumberField()
 
-    course_language_uz=models.CharField(max_length=30,choices=Language_uz,default="O'zbek")
-    course_language_ru=models.CharField(max_length=30,choices=Language_ru,default="Русский")
+    course_language_uz=models.CharField(max_length=30,choices=Language_uz)
+    course_language_ru=models.CharField(max_length=30,choices=Language_ru)
     status=models.CharField(max_length=50,choices=STATUS,default='New')
     course_id=models.ForeignKey(Courses, on_delete=models.CASCADE)
     
     def __str__(self) -> str:
-        return self.name_uz
+        return self.name_uz or self.name_ru
  
--- enrolment/serializers.py
+++ enrolment/serializers.py
@@ -6,16 +6,13 @@ class Enrolmenterializer(serializers.ModelSerializer):
         model=Enrolment
         fields="__all__"
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['course_id'] = instance.courses.all()
-        return representation
+    
 
 class EnrolmentSerializerUz(serializers.ModelSerializer):
     class Meta:
         model=Enrolment
-        fields=['id','name_uz','age','student_number','parents_number','course_language_uz','course_id','status']
+        fields=['id','name_uz','age','student_numer','parents_numer','course_language_uz','course_id','status']
 class EnrolmentSerializerRu(serializers.ModelSerializer):
     class Meta:
         model=Enrolment
-        fields=['id','name_ru','age','student_number','parents_number','course_language_ru','course_id','status']
+        fields=['id','name_ru','age','student_numer','parents_numer','course_language_ru','course_id','status']
